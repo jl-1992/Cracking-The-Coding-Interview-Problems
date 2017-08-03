8eeea456f0657d7ee706b7c658d9773e6afd911b
Joseph Ludy <joseph.earl.ludy@gmail.com>
2017-08-02 20:39:50 -0700

    add files

---
+++ Chapter 8/Problem 8.3/Problem_8_3.java
@@ -0,0 +1,24 @@
+public class Problem_8_3{
+
+	public int magicIndex(int[] a){
+		return magicIndex(a, 0, a.length-1);
+	}
+
+	public int magicIndex(int[] a, int start, int end){
+		int mid= (end+start)/2;
+		if(a.length==0)
+			return -1;
+		if(a[mid]==mid)
+			return mid;
+		if(a[mid]>mid)
+			return magicIndex(a,start,mid);
+		else
+			return magicIndex(a,mid,end);
+		}
+
+	public static void main(String[] args){
+		Problem_8_3 p = new Problem_8_3();
+		int arr[] = {-40,-20,-1,1,2,3,5,7,9,12,13};
+		System.out.println(p.magicIndex(arr));
+	}
+}
